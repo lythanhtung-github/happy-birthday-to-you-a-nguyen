7334f4d1704534ce0f7d1abf372051d22cd94e33
lythanhtung-github <lythanhtung.work@gmail.com>
2024-04-15 20:36:01 +0700

    hpbd

---
+++ config.js
@@ -0,0 +1,26 @@
+var config = {
+    
+    texts: [
+        "Mừng ngày tiếp nối!",
+        "Chúc anh Nguyên sinh nhật vui vẻ",
+        "Thành công",
+        "Hạnh phúc",
+        "Cùng đàn em thơ Chánh Tâm!",
+        "Mai yêu nàaaaa",
+    ],
+    
+    imgs: {
+        "Mừng ngày tiếp nối!": "./imgs/xiaokeai.png", 
+    },
+    
+    desc: {
+        turn_on: "Đen thui à, bật đèn lên đi anh ơi!",
+        play: "Im im rứa, cho tôi con beat đê!",
+        bannar_coming: "Thêm tí màu mè nè!",
+        balloons_flying: "Cho mấy quả bong bóng bay chill chill đi!",
+        cake_fadein: "Cho xin cái bánh kem =))",
+        light_candle: "Thăp nến lên mà thổi, lẹ cái tay đi anh!",
+        wish_message: "Ước lẹ ước lẹ!",
+        story: "Ước rồi thì kích tiếp đi anh!",
+    }
+};
---
+++ readme.md
@@ -0,0 +1,1 @@
+https://lythanhtung-github.github.io/happy-birthday-to-you
